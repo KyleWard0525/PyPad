9a792c83b70f525d3f85d87c2d47acd5106d2ed0
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-11-10 16:34:42 -0500

    Added password encryption for all users

---
+++ models.py
@@ -0,0 +1,40 @@
+from database import db
+from crypto import Crypto
+
+crypt = Crypto()
+
+class Note(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    title = db.Column("title", db.String(200))
+    text = db.Column("text", db.String(100))
+    date = db.Column("data", db.String(50))
+
+    def __init__(self, title, text, date):
+        self.title = title
+        self.text = text
+        self.date = date
+
+class User(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    email = db.Column("email", db.String(100))
+    password = db.Column("password", db.String(200))
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = crypt.encrypt(password)
+
+    def toString(self):
+        print("\nUser: ")
+        print("----------")
+        print("ID: " + str(self.id))
+        print("Name: " + str(self.name))
+        print("Email: " + str(self.email))
+        print("Password: " + str(self.password))
+
+def main():
+    ted = User("ted", "ted@gmail.com", "ted's password")
+    ted.toString()
+
+main()
