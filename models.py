3c27c27cf2cf3de30ec39f573ec8ef6f23a78a3e
LaoSong132 <71239640+LaoSong132@users.noreply.github.com>
2020-12-03 18:58:58 -0500

    models.py - changes to models.py

--- models.py
+++ models.py
@@ -1,55 +1,54 @@
 from database import db
-from crypto import Crypto
+import datetime
 
-crypt = Crypto()
 
 class Note(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     title = db.Column("title", db.String(200))
     text = db.Column("text", db.String(100))
     date = db.Column("date", db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    comments = db.relationship("Comment", backref="note_comment", cascade="all, delete-orphan", lazy=True)
 
-    def __init__(self, title, text, date):
+    # can create a foreign key; referencing the id variable in the User class
+    # so that is why it is lowercase u
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, title, text, date, user_id):
         self.title = title
         self.text = text
         self.date = date
+        self.user_id = user_id
 
 
 class User(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
-    user_name = db.Column("name", db.String(100))
+    first_name = db.Column("first_name", db.String(100))
+    last_name = db.Column("last_name", db.String(100))
     email = db.Column("email", db.String(100))
-    password = db.Column("password", db.String(200))
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    note = db.relationship("Note", backref="note", lazy=True)
+    comments = db.relationship("Comment", backref="user_comment", lazy=True)
 
-    def __init__(self, name, email, password):
-        self.user_name = name
-        self.email = crypt.encrypt(email)
-        self.password = crypt.encrypt(password)
 
-    def toString(self):
-        print("\nUser: ")
-        print("----------")
-        print("id: " + str(self.id))
-        print("user_name: " + str(self.user_name))
-        print("email: " + str(self.email))
-        print("password: " + str(self.password))
-        print("\n")
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.registered_on = datetime.date.today()
 
-    def getPW(self):
-        return self.password
-
-    def getEmail(self):
-        return self.email
 
 class Comment(db.Model):
-    id = db.Column("id", db.Integer, primary_key=True)
-    text = db.Column("text", db.String(100))
-    date = db.Column("date", db.String(50))
-    parent_id = db.Column("parent_id", db.Integer) #ID of note comment belongs to
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.VARCHAR, nullable=False)
+    note_id = db.Column(db.Integer, db.ForeignKey("note.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
-
-    def __init__(self,text,date,parent):
-        self.text = text
-        self.date = date
-        self.parent_id = parent
-    
+    def __init__(self, content, note_id, user_id):
+        self.date_posted = datetime.date.today()
+        self.content = content
+        self.note_id = note_id
+        self.user_id = user_id
